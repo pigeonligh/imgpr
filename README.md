e2420401ce8b039309a2a253fc1712b457bdad62
pigeonligh <pigeonligh@hotmail.com>
2020-04-04 21:03:55 +0800

    update version info

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A Simple Image Process Library (for Digital Image Process Lesson)
 ### Installation
 
 ```
-pip install .
+pip install https://git.pigeonligh.com/pigeonligh/imgpr/-/archive/master/imgpr-master.zip
 ```
--- setup.py
+++ setup.py
@@ -5,18 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="imgpr",
-	version="0.0.1",
-	author="lightning",
-	author_email="cx24321@hotmail.com",
+	version="0.0.2",
+	author="pigeonligh",
+	author_email="pigeonligh@hotmail.com",
 	description="A Simple Image Process Library (for Digital Image Process Lesson)",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://git.pigeonligh.com/pigeonligh/image_process",
+	url="https://git.pigeonligh.com/pigeonligh/imgpr",
 	packages=setuptools.find_packages(),
 	entry_points={},
 	classifiers=[
 		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	]
 )
